600569ee7728896e3cdee80f74f3de1ea6068e66
Pamenarti <pamenarti@gmail.com>
2025-07-18 04:05:00 +0000

    Remove duplicate #[derive(Clone)] from StateStore struct

--- src/storage/state.rs
+++ src/storage/state.rs
@@ -33,6 +33,7 @@ pub trait StateStorage: Send + Sync {
 
 /// State store implementation
 #[derive(Clone)]
+#[derive(Clone)]
 pub struct StateStore {
     database: Database,
     cache: HashMap<Root, BeaconState>,
