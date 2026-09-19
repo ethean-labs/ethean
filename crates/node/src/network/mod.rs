66fd13921bb8bdd0909a70e6382fa087a492af12
Paro <pamenarti@gmail.com>
2026-09-19 08:17:03 +0300

    Re-export StatusSessionBook and blocks-by-root helpers from node network facade.

--- crates/node/src/network/mod.rs
+++ crates/node/src/network/mod.rs
@@ -3,12 +3,13 @@
 //! Legacy Beacon/libp2p modules under this directory are not compiled.
 
 pub use ethean_network::{
-    admit, decode_gossip, delta_for, dial_quic, dial_quic_pending, encode_gossip, handle_status,
+    admit, blocks_by_root_for_status_gap, blocks_by_root_protocol_id, decode_gossip, delta_for,
+    dial_quic, dial_quic_pending, encode_blocks_by_root, encode_gossip, handle_status,
     parse_quic_udp, prepare_transport, probe_udp_status, reject_non_quic, validate_gossip_payload,
     BoundTransport, GossipAction, GossipIngress, LeanGossipTopics, NetworkError, NodeIdentity,
     PeerManager, PeerRecord, PumpEvent, QuicUdpAddr, RequestTracker, Result, StatusExchange,
-    SwarmFacade, TransportConfig, UdpDialProbe, MAX_INBOUND_PEERS, MAX_OUTBOUND_PEERS,
-    MAX_PEERS_PER_IP, SCORE_ACCEPT, SCORE_IGNORE, SCORE_REJECT,
+    StatusSessionBook, SwarmFacade, TransportConfig, UdpDialProbe, MAX_INBOUND_PEERS,
+    MAX_OUTBOUND_PEERS, MAX_PEERS_PER_IP, SCORE_ACCEPT, SCORE_IGNORE, SCORE_REJECT,
 };
 
 #[cfg(feature = "libp2p-quic")]
