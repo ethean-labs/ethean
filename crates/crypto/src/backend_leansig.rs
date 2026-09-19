b34b9d9df6d4edc86fe364529a970982c9eac472
Paro <pamenarti@gmail.com>
2026-09-19 05:36:58 +0300

    Import rand::Rng so ThreadRng fill_bytes resolves under 0.10.

--- crates/crypto/src/backend_leansig.rs
+++ crates/crypto/src/backend_leansig.rs
@@ -6,7 +6,7 @@ use leansig::signature::generalized_xmss::instantiations_aborting::lifetime_2_to
 };
 use leansig::signature::SignatureScheme;
 use rand::rngs::StdRng;
-use rand::{RngCore, SeedableRng};
+use rand::{Rng, SeedableRng};
 
 use crate::error::{CryptoError, Result};
 use crate::signature::{PublicKey, Signature};
