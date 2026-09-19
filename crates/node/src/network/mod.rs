61caa53460fbfa55fec9c5b758a6754aa8f8670b
Paro <pamenarti@gmail.com>
2026-09-19 08:19:40 +0300

    Re-export blocks-by-root outbound helpers from the node network facade.

--- crates/node/src/network/mod.rs
+++ crates/node/src/network/mod.rs
@@ -5,12 +5,13 @@
 pub use ethean_network::{
     admit, blocks_by_root_for_status_gap, blocks_by_root_protocol_id, decode_gossip, delta_for,
     dial_quic, dial_quic_pending, encode_blocks_by_root, encode_gossip, handle_status,
-    parse_quic_udp, prepare_status_outbounds, prepare_transport, probe_udp_status,
-    reject_non_quic, validate_gossip_payload, BoundTransport, GossipAction, GossipIngress,
-    LeanGossipTopics, NetworkError, NodeIdentity, OutboundStatusRequest, PeerManager, PeerRecord,
-    PumpEvent, QuicUdpAddr, RequestId, RequestTracker, Result, StatusExchange, StatusSessionBook,
-    SwarmFacade, TransportConfig, UdpDialProbe, MAX_INBOUND_PEERS, MAX_OUTBOUND_PEERS,
-    MAX_PEERS_PER_IP, SCORE_ACCEPT, SCORE_IGNORE, SCORE_REJECT,
+    parse_quic_udp, prepare_blocks_by_root_outbound, prepare_status_outbounds, prepare_transport,
+    probe_udp_status, reject_non_quic, validate_gossip_payload, BoundTransport, GossipAction,
+    GossipIngress, LeanGossipTopics, NetworkError, NodeIdentity, OutboundBlocksByRootRequest,
+    OutboundStatusRequest, PeerManager, PeerRecord, PumpEvent, QuicUdpAddr, RequestId,
+    RequestTracker, Result, StatusExchange, StatusSessionBook, SwarmFacade, TransportConfig,
+    UdpDialProbe, MAX_INBOUND_PEERS, MAX_OUTBOUND_PEERS, MAX_PEERS_PER_IP, SCORE_ACCEPT,
+    SCORE_IGNORE, SCORE_REJECT,
 };
 
 #[cfg(feature = "libp2p-quic")]
