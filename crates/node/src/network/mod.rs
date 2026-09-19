8ab4a9975baee8a3aeb8d7aa20f5e59ce864b137
Paro <pamenarti@gmail.com>
2026-09-19 05:22:53 +0300

    Re-export BoundTransport and dial_quic from the node network facade.

--- crates/node/src/network/mod.rs
+++ crates/node/src/network/mod.rs
@@ -3,9 +3,9 @@
 //! Legacy Beacon/libp2p modules under this directory are not compiled.
 
 pub use ethean_network::{
-    admit, decode_gossip, delta_for, encode_gossip, handle_status, prepare_transport,
-    reject_non_quic, validate_gossip_payload, GossipAction, NetworkError, NodeIdentity,
-    PeerManager, PeerRecord, RequestTracker, Result, StatusExchange, SwarmFacade, TransportConfig,
-    MAX_INBOUND_PEERS, MAX_OUTBOUND_PEERS, MAX_PEERS_PER_IP, SCORE_ACCEPT, SCORE_IGNORE,
-    SCORE_REJECT,
+    admit, decode_gossip, delta_for, dial_quic, encode_gossip, handle_status, prepare_transport,
+    reject_non_quic, validate_gossip_payload, BoundTransport, GossipAction, NetworkError,
+    NodeIdentity, PeerManager, PeerRecord, RequestTracker, Result, StatusExchange, SwarmFacade,
+    TransportConfig, MAX_INBOUND_PEERS, MAX_OUTBOUND_PEERS, MAX_PEERS_PER_IP, SCORE_ACCEPT,
+    SCORE_IGNORE, SCORE_REJECT,
 };
